9dcb1a3c132b204a6fdec5f5784cfea756eedcfc
wahidans359 <111223481+wahidans359@users.noreply.github.com>
2025-03-14 16:02:20 +0530

    added userProgress

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -5,21 +5,21 @@ type Props = {
     title:string;
     id:number;
     imageSrc:string;
-    onClick: () => void;
+    onClick: (id:number) => void;
     disabled: boolean;
     active: boolean;
 }
 
 export const Card = ({
     title,
-    // id,
+    id,
     imageSrc,
     onClick,    
     disabled,
     active
 } : Props) => {
     return (
-        <div onClick={() => onClick()}
+        <div onClick = {() => onClick(id)}
         className={cn("h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]",
             disabled && "pointer-events-none opacity-50",
         )}>
