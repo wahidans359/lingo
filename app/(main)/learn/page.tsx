8c6a800809bcd3a6dd2070daea4ef3fdc3935a81
wahidans359 <111223481+wahidans359@users.noreply.github.com>
2025-04-12 23:50:34 +0530

    Added Course Progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,20 +2,34 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
-import { getUnits, getUserProgress } from "@/db/queries";
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Unit } from "./unit";
 
 const LearnPage = async () => {
     const userProgressData = getUserProgress();
+    const courseProgressData = getCourseProgress();
+    const lessonPercentageData = getLessonPercentage();
     const unitsData = getUnits();
     
-    const [userProgress,units] = await Promise.all([userProgressData,unitsData]);
+    const [
+        userProgress,
+        units,
+        courseProgress,
+        lessonPercentage
+    ] = await Promise.all([
+        userProgressData,
+        unitsData,
+        courseProgressData,
+        lessonPercentageData
+    ]);
     console.log(units);
     if(!userProgress || !userProgress.activeCourse){
         redirect("/courses");
     }
-
+    if(!courseProgress){
+        redirect("/courses");
+    }
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
@@ -34,8 +48,8 @@ const LearnPage = async () => {
                                 description = {unit.description}
                                 title = {unit.title}
                                 lessons = {unit.lessons}
-                                activeLesson = {undefined}
-                                activeLessonPercentage = {0}
+                                activeLesson = {courseProgress.activeLesson}
+                                activeLessonPercentage = {lessonPercentage}
                                 />
                             }
                         </div>
